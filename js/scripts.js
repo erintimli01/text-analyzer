0e7db73fb26ecaa0c740f2a5bd394168846cf7ff
Erin Timlin <erintimlin@gmail.com>
2023-01-18 10:22:56 -0500

    add new function and more tests

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,21 @@ function wordCounter(text) {
     return 0;
   }
   let wordCount = 0;
-  const textArray = text.split(" ");
+  const textArray = text.split("");
   textArray.forEach(function(element) {
     if (!Number(element)) {
     wordCount++;
+    }
+  });
+  return wordCount;
+}
+
+function numberOfOccurrencesInText(word, text) {
+  const textArray = text.split(" ");
+  let wordCount = 0;
+  textArray.forEach(function(element) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      wordCount++;
     }
   });
   return wordCount;
